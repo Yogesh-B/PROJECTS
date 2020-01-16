9db838f3507c09efc9a7803b67db8fbf5b603792
Yogesh-B <yogeshbavishi199@gmail.com>
2020-01-16 22:33:35 +0530

    Changes to be committed: 	modified:   dotNet/Pract16.cs         modified:   dotNet/Pract16.exe

--- dotNet/Pract16.cs
+++ dotNet/Pract16.cs
@@ -7,34 +7,86 @@ public class Pract16
 {
 	public static void Main()
 	{
-		Console.WriteLine("Enter binary no.:::");
-		double n=double.Parse(Console.ReadLine());
-		int c1=-1,c2=0,r=2;double sum=0;
-		double y=n%1;
-			
-			//integer part
-		while(n!=0)
+		Console.WriteLine("\t\t\t!!!...This is a Number system converter...!!!\n");
+		Numbers num=new Numbers();
+		while(true)
 		{
+			Console.WriteLine("1.Decimal:");
+			Console.WriteLine("2.Binary:");
+			Console.WriteLine("3.Octal:");
+			Console.WriteLine("4.Hexadecimal:");
+			Console.WriteLine("5.Exit:");
+			Console.WriteLine("Enter Source Type:"); 	//get source type
+			int src=int.Parse(Console.ReadLine());
+			if(src==5)
+				break;
 			
-			c1++;
-			int x=(int)n%10; 	//last digit
-			n/=10;			//number changed
-			sum+=(x*Math.Pow(r,c1));
-		
+			Console.WriteLine("Enter Destination Type:");	//get destination type
+			int dst=int.Parse(Console.ReadLine());
+			if(dst==5)
+				break;
+			
+			Console.WriteLine("Enter value:");
+			double val=double.Parse(Console.ReadLine());
+			double result=num.convert(src,dst,val);
+			Console.WriteLine("Answer is = "+result+"\n");
+			
+			
 		}
-			//decimal part
-		while(y!=0)
-		{
-			c2++;	//index
-			y*=10;
-			int x=(int)y/1;	//division
-			y%=1;		//update y
-			sum+=x*(int)Math.Pow(r,-c2);			
-		}
-		Console.Write(sum);
 
 	}
 }
+
+public class Numbers
+{
+	public double convert(int tmpSrc,int tmpDst,double tmpVal)
+	{	enum{A=10,B=11,C=12,D=13,E=14,F=15;}
+		int c1=-1,c2=0,r=0;
+		int[] arr={10,2,8,16};
+		double y,sum=0,temp=0;
+		//To Decimal
+		r=arr[tmpSrc-1];     //set radix point
+		switch(tmpSrc)
+		{
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				y=tmpVal%1;//extract decimal part
+			
+						//integer part
+				while(tmpVal!=0)
+				{
+					c1++;
+					int x=(int)tmpVal%10; 	//last digit
+					tmpVal/=10;			//number changed
+					sum+=(x*Math.Pow(r,c1));
+				}
+						//decimal part
+				while(y!=0)
+				{
+					c2++;	//index
+					y*=10;
+					int x=(int)y/1;	//division
+					y%=1;		//update y
+					sum+=x*Math.Pow(r,-c2);			
+				}
+				
+				
+				break;
+			default:
+				Console.WriteLine("Invalid Input...!!!");
+				break;
+		}
+		
+		
+		
+		
+		return sum;
+	}
+}
+
+
 
 /*OUTPUT:
 
