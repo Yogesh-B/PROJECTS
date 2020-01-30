24039f52de1d9812c252f2b536dade90eef8a108
Yogesh-B <yogeshbavishi199@gmail.com>
2020-01-30 22:40:37 +0530

    JDBC1 file is completed....fully working

--- Java/JDBC1.java
+++ Java/JDBC1.java
@@ -6,7 +6,7 @@ User can create a new database and also create new table under that database. On
  d. Insert Records into respective table..........................#Statement, PreparedStatement, CallableStatement 
  e. Update records of particular table of database .........................#Statement, PreparedStatement, CallableStatement
  f. Delete Records from table .........................................#Statement, PreparedStatement, CallableStatement
- g. Delete table and also database ..................#PreparedStatement
+ g. Delete table and also database ..................#Statement
  
 Use following Java Statement interface to implement program:  
 1. Statement  
@@ -36,25 +36,28 @@ public class JDBC1
         try{
             
             Class.forName(JDBC_driver);
+                    /*(a)connect to database*/
             conn=DriverManager.getConnection(url,usr,passwd);
             String sql;
-            Scanner sc=new Scanner(System.in);
-                    /*(a)create database*/ 
+                    
+                    /*(b)create database*/ 
             stmt=conn.createStatement();
-            System.out.println("Enter 1 & Press Enter To create database & table using Statement");
+            System.out.println("Press Enter To create database & table using Statement");
+            System.in.read();
+            System.in.skip(System.in.available());
             System.out.println("Creating database and table using Statement...");
             sql="CREATE DATABASE demo";
             stmt.executeUpdate(sql);
             stmt.executeUpdate("USE demo");
             
-                    /*(b)create table*/
+                    /*(c)create table*/
             sql="CREATE TABLE student (Enroll BIGINT(12),Name VARCHAR(30),Department VARCHAR(10),Semester INT)";
             stmt.executeUpdate(sql);
-            System.out.println("CREATED DATABASE AND TABLE USING Statement...\nEnter 1 and press enter to continue...");
-            int n=sc.nextInt();//for getting EnterKey
-                   
-                   
-                   /*(c)insert records using Statement,PreparedStatement,CallableStatement*/
+            System.out.println("CREATED DATABASE AND TABLE USING Statement...\nPress enter to INSERT...");
+            System.in.read();                   
+            System.in.skip(System.in.available());
+            
+                   /*(d)insert records using Statement,PreparedStatement,CallableStatement*/
                 //...using Statement
             System.out.println("\n\nInserting data...");
             stmt.executeUpdate("INSERT INTO student VALUES(1702001001,'Atul','CE',3)");
@@ -76,10 +79,12 @@ public class JDBC1
             cstmt.setString(3,"IC");
             cstmt.setInt(4,6);
             cstmt.executeUpdate();
-            System.out.println("Data Inserted Using CallableStatement...");
+            System.out.println("Data Inserted Using CallableStatement...\nPress enter to UPDATE...");
+            System.in.read();
+            System.in.skip(System.in.available());
                 
                 
-                    /*(d)update  records using Statement,PreparedStatement,CallableStatement*/
+                    /*(e)update  records using Statement,PreparedStatement,CallableStatement*/
                 //...using Statement
             System.out.println("\n\nUpdating data...");
             stmt.executeUpdate("UPDATE student SET Semester=08 WHERE Enroll=1702001001");
@@ -95,18 +100,19 @@ public class JDBC1
             sql = "call demo.Change(?,?)";
             cstmt=conn.prepareCall(sql);
             cstmt.setInt(1,1702001003);
-            cstmt.setString(2,06); 
+            cstmt.setInt(2,06); 
             cstmt.executeUpdate();
-            System.out.println("Data Updated Using CallableStatement...");
- 
+            System.out.println("Data Updated Using CallableStatement...\nPress enter to DELETE...");
+            System.in.read();                   
+            System.in.skip(System.in.available());
             
-                    /*(e)delete  records using Statement,PreparedStatement,CallableStatement*/
+                    /*(f)delete  records using Statement,PreparedStatement,CallableStatement*/
                 //...using Statement
             System.out.println("\n\nDeleting data...");
             stmt.executeUpdate("DELETE FROM Student WHERE Enroll=1702001001");
             System.out.println("Data Deleted Using Statement...");
                 //...using PreparedStatement 
-            sql = "DELETE FROM Student WHERE Enroll=?;";
+            sql = "DELETE FROM Student WHERE Enroll=?";
             pstmt = conn.prepareStatement(sql);
             pstmt.setInt(1,1702001002);
             pstmt.executeUpdate();
@@ -116,9 +122,19 @@ public class JDBC1
             cstmt=conn.prepareCall(sql);
             cstmt.setInt(1,1702001003);
             cstmt.executeUpdate();
-            System.out.println("Data Deleted Using CallableStatement...");
+            System.out.println("Data Deleted Using CallableStatement...\nPress enter to DELETE TABLE...");
+            System.in.read();                  
+            System.in.skip(System.in.available());
             
-            
+                    /*(g)delete table and database*/
+            stmt.executeUpdate("DROP TABLE student");
+            System.out.println("Table Deleted Using Statement...\nPress enter to DELETE DATABASE...");
+            System.in.read();             
+            System.in.skip(System.in.available());
+            stmt.executeUpdate("DROP DATABASE demo");
+            System.out.println("Database Deleted Using Statement...");
+            System.in.read();                   
+            System.in.skip(System.in.available());
             
             
             //closing all objects
